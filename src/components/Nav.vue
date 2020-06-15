6e95840d9f0bf6e7777a1db671439612158166b3
DK <darshankhot@programmer.net>
2020-06-15 13:39:27 +0530

    login  testing for node server

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -21,7 +21,7 @@
 
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a href="#" class="nav-link">Login</a>
+            <a href="/login" class="nav-link">Login</a>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">Register</a>
