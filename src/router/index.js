bbd03210b1de0a4ed48872656fb85c0d47939a81
DK <darshankhot@programmer.net>
2020-06-17 12:19:26 +0530

    removed pull lines

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,8 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 import Home from '../views/Home'
-<<<<<<< HEAD
 import Contact from '../views/Contact'
 import About from '../views/About'
-=======
-import About from '../views/About'
-import Contact from '../views/Contact'
->>>>>>> 2c35a3e22e53fd51118947f8b71cb33ace6d2cb2
 
 export default new VueRouter({
   mode: 'history',
